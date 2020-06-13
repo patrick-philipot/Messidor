9c74435f24ae2023db417609eda3367372ba3836
pat <patrick.philipt@gmail.com>
2020-06-13 09:07:44 +0200

    Initial Commit

---
+++ Messidor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Messidor
+//
+//  Created by patrick philipot on 13/06/2020.
+//  Copyright © 2020 stgpcs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
